e8e4c238fcf1a5409b2117a89c06251c56b17de2
Sazzad Hosen <sajjadhosenmahim@gmail.com>
2023-05-12 12:07:16 +0600

    fool and final basic node mongo setup

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-// geniusDBUser
-// ixVPSHYifb1X5CkF
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3rsuy99.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,22 +19,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Genius car server is running");
